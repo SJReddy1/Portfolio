f51b25203495e43631dcee2b56f4ec8576c0ed1a
SJReddy1 <122619223+SJReddy1@users.noreply.github.com>
2023-12-21 17:26:24 +0530

    Add files via upload

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+    //   {
+    //     title: "Consultant",
+    //     subTitle: "Miami, FL",
+    //     description:
+    //       " User Experience, Visual Design",
+    //   },
+    //   {
+    //     title: "Senior Consultant",
+    //     subTitle: "London, UK",
+    //     description:
+    //       "Web development, Databases, Backend developement",
+    //   },
+    //   {
+    //     title: "Project Manager",
+    //     subTitle: "Bangalore, INDIA",
+    //     description:
+    //       "Creative Direction Project Management, Team Leading",
+    //   },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "St Rossello's School",
+        description:
+          "10th Grade 2017-18" ,
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "Shri Nirala College",
+        description:
+          "12th Grade in Science 2019-20",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "PDPM IIITDM Jabalpur",
+        description:
+          "Bachelor of technology in computer science 2020-2024",
+      },
+    ],
+  };
